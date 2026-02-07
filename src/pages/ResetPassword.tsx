e6853da5c79c80cd9fac6792a94d53b6157346ec
Muhammad Raza <raza.dev145@gmail.com>
2026-02-07 18:52:22 +0500

    feedback history

--- src/pages/ResetPassword.tsx
+++ src/pages/ResetPassword.tsx
@@ -113,7 +113,7 @@ const ResetPassword = () => {
               </Button>
             </form>
 
-            <div className="mt-6 text-center">
+            <div className="mt-3 text-center">
               <p className="text-sm text-gray-600">
                 Remember your password?{" "}
                 <Link to="/login" className="text-purple-600 hover:text-purple-700 font-medium">
